fe16bfe90df6c4870b610a04c154140bef7c6807
Enzo POGGIO <poggio.enzo@gmail.com>
2019-04-23 00:09:10 +0200

    comment test

--- lexer/lex_main.py
+++ lexer/lex_main.py
@@ -10,10 +10,10 @@ def main():
 	else :
 		lc = ListCommands(" ".join(sys.argv[1:]))
 	# code.interact(local = locals())
-	print([ lc.tokens[x.start:x.end] for x in lc.tree_commands])
-	print([ (x.start,x.end) for x in lc.tree_commands])
-	print([(x.sub[0].start, x.sub[0].end) if x.sub != [] else () for x in lc.tree_commands ])
-	print([ x.sub for x in lc.tree_commands ])
+	# print([ lc.tokens[x.start:x.end] for x in lc.tree_commands])
+	# print([ (x.start,x.end) for x in lc.tree_commands])
+	# print([(x.sub[0].start, x.sub[0].end) if x.sub != [] else () for x in lc.tree_commands ])
+	# print([ x.sub for x in lc.tree_commands ])
 
 
 if __name__ == '__main__':
