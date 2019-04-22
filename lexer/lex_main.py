fc583db8b68d73ac7c0dd5bc01d9c97a2789e0f0
Enzo POGGIO <poggio.enzo@gmail.com>
2019-04-22 23:42:54 +0200

    display for subtest

--- lexer/lex_main.py
+++ lexer/lex_main.py
@@ -6,12 +6,14 @@ import code
 
 def main():
 	if len(sys.argv) == 1:
-		lc = ListCommands("ls -l; echo 'lol' 1;$(echo ls)\\\ncat ../*")
+		lc = ListCommands("ls -l; echo 'lol' 1;$(echo ls) \\\ncat ../*")
 	else :
-		print(" ".join(sys.argv[1:]))
 		lc = ListCommands(" ".join(sys.argv[1:]))
-	#code.interact(local = locals())
+	# code.interact(local = locals())
 	print([ lc.tokens[x.start:x.end] for x in lc.tree_commands])
+	print([ (x.start,x.end) for x in lc.tree_commands])
+	print([(x.sub[0].start, x.sub[0].end) if x.sub != [] else () for x in lc.tree_commands ])
+	print([ x.sub for x in lc.tree_commands ])
 
 
 if __name__ == '__main__':
