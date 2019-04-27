ed8245d5b92cff0db86046b84c9a7eac65a65232
AbcSxyZ <rossi.sim@outlook.com>
2019-04-27 22:00:57 +0200

    remove useless function

--- src/history/history_manager.c
+++ src/history/history_manager.c
@@ -5,23 +5,6 @@
 #include "char_utils.h"
 #include "cursor_dependent_selection.h"
 
-
-/*
-** check_last_key:
-**
-** Analyse the previous typed char to check if the current history
-** selection is a repetition. If it a new history usage, set up the
-** history pointer to the last node of the chained list.
-*/
-
-static void	check_last_key(int *is_repetition)
-{
-	if (DOWN_KEY(g_last_char) || UP_KEY(g_last_char))
-        *is_repetition = TRUE;
-	else
-		*is_repetition = FALSE;
-}
-
 /*
 ** history_manager:
 **
@@ -37,7 +20,7 @@ void	history_manager(char *key, t_line *shell_repr, t_cursor *cursor)
 	static t_hist	*history = (void *) 0;
     t_line          *cursor_line;
 
-	check_last_key(&is_repetition);
+	is_repetition = DOWN_KEY(g_last_char) || UP_KEY(g_last_char);
     clean_lastest_history(is_repetition, shell_repr, cursor, history);
     select_current_history(key, &history, is_repetition);
     add_new_history(shell_repr, cursor, history);
