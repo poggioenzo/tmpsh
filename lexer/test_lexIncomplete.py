aea8246f5fe69aa02873d558f80c0ffef1592760
Enzo POGGIO <poggio.enzo@gmail.com>
2019-05-07 20:00:35 +0200

    test which fails

--- lexer/test_lexIncomplete.py
+++ lexer/test_lexIncomplete.py
@@ -40,7 +40,6 @@ class testLexIncomplete(unittest.TestCase):
         self.assertTrue(listc.incomplete)
         self.assertListEqual(['DQUOTES', '>'], listc.to_complete)
 
-
     def testLexIncompleter006(self):
         term_inputs = '" \\"""'
         listc = ListCommands(term_inputs)
@@ -48,13 +47,44 @@ class testLexIncomplete(unittest.TestCase):
         self.assertTrue(listc.incomplete)
         self.assertListEqual(['DQUOTES', '>'], listc.to_complete)
 
-
     def testLexIncompleter007(self):
         term_inputs = '\''
         listc = ListCommands(term_inputs)
         self.assertTrue(listc.valid)
         self.assertTrue(listc.incomplete)
         self.assertListEqual(['QUOTE', '>'], listc.to_complete)
+
+    def testLexIncompleter008(self):
+        term_inputs = 'ls -l \\'
+        listc = ListCommands(term_inputs)
+        self.assertTrue(listc.valid)
+        self.assertTrue(listc.incomplete)
+        self.assertListEqual(['>'], listc.to_complete)
+
+    def testLexIncompleter009(self):
+        term_inputs = '\\'
+        listc = ListCommands(term_inputs)
+        self.assertTrue(listc.valid)
+        self.assertTrue(listc.incomplete)
+        self.assertListEqual(['>'], listc.to_complete)
+
+    def testLexIncompleter010(self):
+        term_inputs = '${ $( 	{ ( ( ) >( <(  '
+        listc = ListCommands(term_inputs)
+        self.assertTrue(listc.valid)
+        self.assertTrue(listc.incomplete)
+        self.assertListEqual(['BRACEPARAM', 'CMDSUBST1', 'CURSH', 'SUBSH',
+                              'CMDSUBST2', 'CMDSUBST3', '>'],
+                             listc.to_complete)
+
+
+    def testLexIncompleter011(self):
+        term_inputs = "' $("
+        listc = ListCommands(term_inputs)
+        self.assertTrue(listc.valid)
+        self.assertTrue(listc.incomplete)
+        self.assertListEqual(['QUOTE', '>'],
+                             listc.to_complete)
 
 
 
