89dbf842aa43b2e00ac22493f6113bf051fdc5c7
Enzo POGGIO <poggio.enzo@gmail.com>
2019-06-08 20:18:08 +0200

    new init methode for TagsTokens

--- lexer/lex_main.py
+++ lexer/lex_main.py
@@ -7,8 +7,8 @@ from utils.tagstokens import TagsTokens
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        TAGSTOKENS = TagsTokens(
+        TAGSTOKENS = TagsTokens().init_with_input(
             "ls -l; echo 'lol' 1;$(echo ls) \\\ncat ../*")
     else:
-        TAGSTOKENS = TagsTokens(" ".join(sys.argv[1:]))
+        TAGSTOKENS = TagsTokens().init_with_input(" ".join(sys.argv[1:]))
     print(TAGSTOKENS)
--- lexer/utils/tagstokens.py
+++ lexer/utils/tagstokens.py
@@ -1,17 +1,17 @@
+#! /usr/bin/env python3
+
 import utils.global_var as gv
 import utils.tokenizer as tk
 import utils.strcontain as sc
 
 
 class TagsTokens():
-    def __init__(self, term_inputs, ):
-        self.term_inputs = term_inputs
-        self.tokens = []
-        tk.tokenize(term_inputs, self.tokens)
-        self.tags = []
-        self.get_tags()
+    def __init__(self, tags=None, tokens=None):
+        self.tokens = tags if tags else []
+        self.tags = tokens if tokens else []
 
-    def get_tags(self):
+    def init_with_input(self, term_inputs):
+        tk.tokenize(term_inputs, self.tokens)
         for tok in self.tokens:
             if tok in gv.GRAMMAR.leaf_op:
                 self.tags.append(gv.GRAMMAR.reverse[tok])
@@ -19,6 +19,7 @@ class TagsTokens():
                 self.tags.append('SPACES')
             else:
                 self.tags.append('STMT')
+        return self
 
     def __str__(self):
         import pandas as pd
