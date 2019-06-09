c4741ec55002904d47c8da3c84d903f73934b726
AbcSxyZ <rossi.sim@outlook.com>
2019-06-09 18:58:08 +0200

    change folder

---
+++ concept_example/pipe.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+
+int		display_error(char *err_msg, int status)
+{
+	perror(err_msg);
+	return (status);
+}
+
+/*
+** Create a pipe, and fork the program to have 2 process who communicate together.
+** Equal to : top | grep zsh
+*/
+
+int		main(int argc, char **argv, char **environ)
+{
+	int		fd_pipe[2];
+	char	*argv_grep[] = {"/bin/grep", "zsh", NULL};
+	pid_t	pid;
+
+	if (pipe(fd_pipe) == -1)
+		return (display_error("pipe", 1));
+    
+	if ((pid = fork()) == -1)
+		return (display_error("fork", 1));
+	if (pid == 0)
+	{
+		printf("top run in background\n");
+		dup2(fd_pipe[1], 1);
+		execve("/usr/bin/top", argv, environ);
+	}
+	else
+	{
+		dup2(fd_pipe[0], 0);
+		printf("run grep\n");
+		execve("/usr/bin/grep", argv_grep, environ);
+	}
+  return (0);
+}
