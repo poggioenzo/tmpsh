05dcb18ad8193a32fe0c89dd2c5387c0afc2f001
Enzo POGGIO <poggio.enzo@gmail.com>
2019-06-10 14:23:50 +0200

    rename files

--- pyshell/test_syntax.py
+++ pyshell/test_syntax.py
@@ -4,19 +4,28 @@ import unittest
 from utils.commands import ListCommands
 
 
-class testLexError(unittest.TestCase):
+class TestSyntaxValid(unittest.TestCase):
 
-    def testLexError001(self):
+    def test_syntax_valid_001(self):
+        term_inputs = ''
+
+
+class TestSyntaxError(unittest.TestCase):
+
+    def test_syntax_error_001(self):
         term_inputs = '&&'
         listc = ListCommands(term_inputs)
         self.assertFalse(listc.valid)
         self.assertIs('&&', listc.error)
 
-    def testLexError002(self):
-        term_inputs = 'cmd && ;'
+
+class TestSyntaxIncomplete(unittest.TestCase):
+
+    def test_syntax_incomplete_001(self):
+        term_inputs = '&&'
         listc = ListCommands(term_inputs)
         self.assertFalse(listc.valid)
-        self.assertIs(';', listc.error)
+        self.assertIs('&&', listc.error)
 
 
 if __name__ == '__main__':
