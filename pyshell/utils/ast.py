461c8beb58882349360769a52a1f155859632a1f
EPgg92 <poggio.enzo@gmail.com>
2019-06-12 01:06:54 +0200

    need correcton for deep tree

--- pyshell/utils/ast.py
+++ pyshell/utils/ast.py
@@ -4,12 +4,14 @@ import utils.global_var as gv
 from utils.tagstokens import TagsTokens as TT
 
 
-def split_shift(tabit):
-    return ''.join(['\t{}'.format(x) for x in tabit])[: -1]
+def split_shift(string):
+    return '\n'.join(['\t{}'.format(x) for x in string.split('\n')])[:-1]
 
 
-def wait(waiting):
-    return len(waiting) > 0 and waiting[-1] == ''
+def wait(waiting, tag=''):
+    if tag != '':
+        return len(waiting) > 0 and waiting[-1] == tag
+    return waiting == []
 
 
 class AST():  # AbstractSyntaxTree
@@ -18,6 +20,10 @@ class AST():  # AbstractSyntaxTree
     def __init__(self, tagstokens):
         self.list_branch = []
         self.split_branch(tagstokens)
+        self.type = 'ROOT'
+
+    def set_type(self, type):
+        self.type = type
 
     def split_branch(self, tt):
         i = 0
@@ -25,66 +31,70 @@ class AST():  # AbstractSyntaxTree
         and_or_begin = ''
         wtg = []  # for waiting
         tag = ''
-        while i <= tt.length:
-            if i < tt.length:
-                tag = tt.tags[i]
-            if wait(wtg):
+        while i < tt.length:
+            tag = tt.tags[i]
+            if wait(wtg, tag):
                 wtg.pop(-1)
             elif tag in gv.GRAMMAR.opening_tags:
                 wtg.append(gv.GRAMMAR.opening_tags[tag])
-            if (wait(wtg) and tag in gv.GRAMMAR.grammar['ABS_TERMINATOR'])\
-                    or i == tt.length - 1:
-                self.list_branch.append(ACB(TT(tt.tokens[begin:i + 1],
-                                               tt.tags[begin:i + 1]),
+            elif (wait(wtg) and tag in gv.GRAMMAR.grammar['ABS_TERMINATOR']):
+                self.list_branch.append(ACB(TT(tt.tokens[begin:i],
+                                               tt.tags[begin:i]),
                                             and_or_begin,
-                                            tag == 'PIPE'))
+                                            tag))
                 begin = i + 1
                 and_or_begin = ''
             if tag in ['CMDAND', 'CMDOR']:
                 and_or_begin = tag
             i += 1
+        self.list_branch.append(ACB(TT(tt.tokens[begin:i], tt.tags[begin:i]),
+                                    and_or_begin,
+                                    tag))
 
     def __str__(self):
-        return '\n'.join([str(branch) for branch in self.list_branch])
+        return '{:_^10}:\n'.format(self.type) + split_shift('\n'.join(
+            [str(branch) for branch in self.list_branch]))
 
 
 class ACB():  # AbstractCommandBranch
     """docstring for AbstractSyntaxTree."""
 
-    def __init__(self, tt, begin_andor, pipe_end):
+    def __init__(self, tt, begin_andor, tag_end):
         self.tagstokens = tt
         self.begin_andor = begin_andor
-        self.pipe_end = pipe_end
-        self.type = 'NORMAL'
-        if len(tt.tags) > 0 and tt.tags[0] in gv.GRAMMAR.opening_tags:
-            self.type = tt.tags[0]
-        self.subcmd = []  # list of AST
+        self.tag_end = tag_end
+        self.subast = []  # list of AST
+        self.subcmd_type = []
         self.check_subcmd()
+        # self.set_subast_type()
+
+    def set_subast_type(self):
+        for type, sub_ast in zip(self.subcmd_type, self.subast):
+            sub_ast.set_type(type)
 
     def check_subcmd(self):
         i = 0
         begin = 0
         wtg = []  # for waiting
         tag = ''
-        while i <= self.tagstokens.length:
-            if i < self.tagstokens.length:
-                tag = self.tagstokens.tags[i]
-            if len(wtg) > 0 and tag == wtg[-1]:
-                wtg.pop(-1)
+        while i < self.tagstokens.length:
+            tag = self.tagstokens.tags[i]
             if tag in gv.GRAMMAR.opening_tags:
                 wtg.append(gv.GRAMMAR.opening_tags[tag])
-                begin = i
-            elif len(wtg) > 0 and tag == wtg[-1]:
-                self.subcmd.append(AST(TT(self.tagstokens.tokens[begin:i],
-                                          self.tagstokens.tags[begin:i])))
+                begin = i + 1
+            elif wait(wtg, tag):
+                if len(wtg) == 1:
+                    self.subast.append(AST(TT(self.tagstokens.tokens[begin:i],
+                                              self.tagstokens.tags[begin:i])))
+                wtg.pop(-1)
+            else:
+                print(wait(wtg, tag), wtg, tag)
             i += 1
 
     def __str__(self):
-        cmd = '{:_<10}:'.format(self.type)
-        cmd += '{:_<4}'.format(self.begin_andor)
+        cmd = '{:_^10}'.format(self.begin_andor)
         cmd += '{}'.format(''.join(self.tagstokens.tokens))
-        if self.pipe_end:
-            cmd += 'PIPE'
-        if self.subcmd != []:
-            cmd += '\n'.join([str(cmd) for cmd in self.subcmd])
+        cmd += '{:_^15}\n'.format(self.tag_end)
+        if self.subast != []:
+            cmd += split_shift('\n'.join([str(cmd) for cmd in self.subast]))
         return cmd
