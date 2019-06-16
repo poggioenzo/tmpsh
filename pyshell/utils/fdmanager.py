68f9a9b2ee52eb7cf7202bed094a500413150db4
Enzo POGGIO <poggio.enzo@gmail.com>
2019-06-16 20:10:27 +0200

    comment useless

--- pyshell/utils/fdmanager.py
+++ pyshell/utils/fdmanager.py
@@ -1,4 +1,11 @@
 
+class FileDescriptor():
+    """docstring for FileDescriptor."""
+
+    def __init__(self):
+        self.arg = arg
+
+
 class FDManager():
     """docstring for FDManager."""
 
@@ -10,11 +17,11 @@ class FDManager():
         self.fd_trunc_to_fd = []  # >&
         self.fd_read_from_fd = []  # <&
 
-    def get_redirection_list_from_tag(self, tag):
-        redirection_lists = {'APPEND': self.fd_append,
-                             'TRUNC': self.fd_trunc,
-                             'HEREDOC': self.fd_heredoc,
-                             'READ_FROM': self.fd_read_from,
-                             'TRUNC_TO_FD': self.fd_trunc_to_fd,
-                             'READ_FROM_FD': self.fd_read_from_fd}
-        return redirection_lists[tag] if tag in redirection_lists else None
+    # def get_redirection_list_from_tag(self, tag):
+    #     redirection_lists = {'APPEND': self.fd_append,
+    #                          'TRUNC': self.fd_trunc,
+    #                          'HEREDOC': self.fd_heredoc,
+    #                          'READ_FROM': self.fd_read_from,
+    #                          'TRUNC_TO_FD': self.fd_trunc_to_fd,
+    #                          'READ_FROM_FD': self.fd_read_from_fd}
+    #     return redirection_lists[tag] if tag in redirection_lists else None
