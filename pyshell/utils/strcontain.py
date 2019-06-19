de1b735567769d64b1f5b5652f24467eec0472a2
Enzo POGGIO <poggio.enzo@gmail.com>
2019-06-19 13:58:43 +0200

    create an containonlyspaces

--- pyshell/utils/strcontain.py
+++ pyshell/utils/strcontain.py
@@ -2,6 +2,10 @@ def containspaces(key):
     return any([l in [' ', '\t'] for l in key])
 
 
+def containonlyspaces(key):
+    return all([l in [' ', '\t'] for l in key])
+
+
 def containalphanum(key):
     return any([l.isalnum() for l in key])
 
