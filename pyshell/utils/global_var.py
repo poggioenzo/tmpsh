0447050b2c055f57e9e4156420c3bf1d3c1be742
Enzo POGGIO <poggio.enzo@gmail.com>
2019-06-19 16:18:03 +0200

    update dict

--- pyshell/utils/global_var.py
+++ pyshell/utils/global_var.py
@@ -1,7 +1,7 @@
 from utils.shellgrammar import ShellGrammar
 
 GRAMMAR = ShellGrammar()
-ALIAS = {'all': 'ls -l', 'gall': 'all -G', 'ball': '$(gall)',
+ALIAS = {'all': 'ls -l', 'gall': 'all -G -1', 'ball': '$(gall)',
          'recursion1': 'recursion2',
          'recursion2': 'recursion3',
          'loop1': 'loop2',
