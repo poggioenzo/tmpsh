04d0383b7bacbb68194700fd914ad7266a0bedf4
Enzo POGGIO <poggio.enzo@gmail.com>
2019-06-20 16:16:12 +0200

    new gesture for tagstokens exeption

---
+++ pyshell/utils/tagstokensmonitor.py
@@ -0,0 +1,29 @@
+# def update_i(func):
+#     def method(self):
+#
+
+
+class TagsTokensMonitor():
+    """docstring forTagsTokensMonitor."""
+
+    def __init__(self, tt):
+        self.tt = tt
+        self.i = 0
+        self.depth = 0
+
+    def op_selector(self):
+        pass
+
+    def is_quote(self):
+        pass
+
+    def is_dquote(self):
+        pass
+
+
+# QUOTE
+# DQUOTES
+# BRACEPARAM
+# in_SUB_PROCESS (CMDSUBST1, CMDSUBST2, CMDSUBST3)
+# in_COMMAND_SH
+# in_REDIRECTION
