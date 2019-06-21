2105a0cc2dbfdfc8ebc87df9eb8a0776319fcab8
AbcSxyZ <rossi.sim@outlook.com>
2019-06-21 17:01:08 +0200

    add work on cmdsubst

--- pyshell/utils/execute.py
+++ pyshell/utils/execute.py
@@ -2,6 +2,8 @@
 
 from utils.tagstokens import TagsTokens as Token
 import utils.global_var as gv
+import fcntl
+import sys
 import os
 
 def get_execname(cmd):
@@ -10,6 +12,7 @@ def get_execname(cmd):
         execname = os.path.join(folder, cmd)
         if os.path.isfile(execname):
             return execname
+    print("no exec")
     return None
 
 
@@ -27,6 +30,9 @@ class Executor:
         pid = os.getpid()
         while index < nbr_branch and pid > 0:
             branch = ast.list_branch[index]
+            if self.check_andor(branch) == False:
+                index = self.find_newstart(nbr_branch, index, ast)
+                continue
             #Check if the command have to be launched in background
             if branch.tag_end == "BACKGROUND_JOBS":
                 pid = self.run_background_process(branch)
@@ -35,9 +41,6 @@ class Executor:
                     continue
             if self.perfom_subast_command(branch) == True:
                 index += 1
-                continue
-            if self.check_andor(branch) == False:
-                index = self.find_newstart(nbr_branch, index, ast)
                 continue
             self.perform_subast_replacement(branch)
             cmd = self.extract_cmd(branch)
@@ -56,6 +59,53 @@ class Executor:
             os.setpgid(0, 0)
         return pid
 
+    def prepare_substitution_pre_fork(self, type_subst):
+        """
+        When a CMDSUBST is done, prepare his fd according to the expected
+        configuration.
+        """
+        read_fd, write_fd = os.pipe()
+
+    def prepare_substitution_fd(self, type_subst, pipe_fd):
+        if type_subst == "CMDSUBST1":
+            os.dup2(pipe_fd[1], sys.stdout.fileno())
+        elif type_subst == "CMDSUBST2":
+            os.dup2(pipe_fd[0], sys.stdin.fileno())
+        elif type_subst == "CMDSUBST3":
+            os.dup2(pipe_fd[1], sys.stdout.fileno())
+
+    def configure_pipe_fd(self, pipe_fd, non_blocking=False):
+        """"""
+        #Change fd value to get fd in range of [63:infinite[
+        old_pipe = pipe_fd.copy()
+        pipe_fd[0] = fcntl.fcntl(pipe_fd[0], fcntl.F_DUPFD, 63)
+        pipe_fd[1] = fcntl.fcntl(pipe_fd[1], fcntl.F_DUPFD, 63)
+        os.set_inheritable(pipe_fd[0], True)
+        os.set_inheritable(pipe_fd[1], True)
+        os.close(old_pipe[0])
+        os.close(old_pipe[1])
+        if non_blocking:
+            opts = fcntl.fcntl(pipe_fd[0], fcntl.F_GETFL)
+            fcntl.fcntl(pipe_fd[0], fcntl.F_SETFL, opts | os.O_NONBLOCK)
+            opts = fcntl.fcntl(pipe_fd[1], fcntl.F_GETFL)
+            fcntl.fcntl(pipe_fd[1], fcntl.F_SETFL, opts | os.O_NONBLOCK)
+
+    def run_subshell(self, subast):
+        """Run an ast in a subshell"""
+        if subast.type in ["CMDSUBST1", "CMDSUBST2", "CMDSUBST3"]:
+            pipe_fd = list(os.pipe())
+            non_blocking = subast.type == "CMDSUBST1"
+            self.configure_pipe_fd(pipe_fd, non_blocking=non_blocking)
+        pid = os.fork()
+        if subast.type in ["CMDSUBST1", "CMDSUBST2", "CMDSUBST3"] and pid == 0:
+            self.prepare_substitution_fd(subast.type, pipe_fd)
+        if pid == 0:
+            self.run_ast(subast)
+            exit(gv.LAST_STATUS)
+        else:
+            if subast.type in ["CMDSUBST1", "SUBSH", "CMDSUBST3"]:
+                self.analyse_status(pid)
+
     def perfom_subast_command(self, branch):
         """Run each command of subast which are not variable replacement"""
         index = 0
@@ -63,16 +113,11 @@ class Executor:
         runned = False
         while index < nbr_subast:
             subast = branch.subast[index]
-            if subast.type == "SUBSH":
-                pid = os.fork()
-                if pid == 0:
-                    self.run_ast(subast)
-                else:
-                    self.analyse_status(pid)
-                    exit(gv.LAST_STATUS)
-                runned = True
+            if subast.type in ["SUBSH", "CMDSUBST1", "CMDSUBST2", "CMDSUBST3"]:
+                self.run_subshell(subast)
             if subast.type == "CURSH":
                 self.run_ast(subast)
+            if subast.type in ["CURSH", "SUBSH"]:
                 runned = True
             index += 1
         return runned
