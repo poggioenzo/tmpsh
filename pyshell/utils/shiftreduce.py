946ac4a2fd9c2ed4956d5d720652e5a338989773
Enzo POGGIO <poggio.enzo@gmail.com>
2019-07-11 18:43:42 +0200

    comment function for production of conflict rule

--- pyshell/utils/shiftreduce.py
+++ pyshell/utils/shiftreduce.py
@@ -38,35 +38,35 @@ def reduce_all(stack, instack, grammar):
     return stack
 
 
-def check_forbidden(tags, grammar):
-    i = 0
-    len_tags = len(tags)
-    instack = []
-    key = ''
-    spaces = 0
-    while i < len_tags:
-        key = ' '.join(instack)
-        if key in grammar.reverse and grammar.reverse[key] == 'FORBIDDEN':
-            if tags[i - len(instack)] == 'SPACES':
-                tags[i - len(instack) - 1] = 'FORBIDDEN'
-            else:
-                tags[i - len(instack)] = 'FORBIDDEN'
-            spaces = 0
-            instack = []
-            break
-        elif tags[i] == 'SPACES':
-            spaces += 1
-        elif instack == [] or tk.ops_begin_with(
-                key, grammar.grammar['FORBIDDEN']):
-            instack.append(tags[i])
-        else:
-            instack = []
-        i += 1
-    key = ' '.join(instack)
-    if key in grammar.reverse and grammar.reverse[key] == 'FORBIDDEN':
-        tags[i + spaces - len(instack)] = 'FORBIDDEN'
-        spaces = 0
-    return tags
+# def check_forbidden(tags, grammar):
+#     i = 0
+#     len_tags = len(tags)
+#     instack = []
+#     key = ''
+#     spaces = 0
+#     while i < len_tags:
+#         key = ' '.join(instack)
+#         if key in grammar.reverse and grammar.reverse[key] == 'FORBIDDEN':
+#             if tags[i - len(instack)] == 'SPACES':
+#                 tags[i - len(instack) - 1] = 'FORBIDDEN'
+#             else:
+#                 tags[i - len(instack)] = 'FORBIDDEN'
+#             spaces = 0
+#             instack = []
+#             break
+#         elif tags[i] == 'SPACES':
+#             spaces += 1
+#         elif instack == [] or tk.ops_begin_with(
+#                 key, grammar.grammar['FORBIDDEN']):
+#             instack.append(tags[i])
+#         else:
+#             instack = []
+#         i += 1
+#     key = ' '.join(instack)
+#     if key in grammar.reverse and grammar.reverse[key] == 'FORBIDDEN':
+#         tags[i + spaces - len(instack)] = 'FORBIDDEN'
+#         spaces = 0
+#     return tags
 
 
 def shift_reduce(tags, grammar):
@@ -93,7 +93,7 @@ def shift_reduce(tags, grammar):
 def tagstokens_shift_reduce(tagstokens, grammar):
     stack = []
     i = 0
-    tagstokens.tags = check_forbidden(tagstokens.tags, grammar)
+    # tagstokens.tags = check_forbidden(tagstokens.tags, grammar)
     len_tags = len(tagstokens.tags)
     while i <= len_tags:
         instack = keyinstack(stack, grammar)
