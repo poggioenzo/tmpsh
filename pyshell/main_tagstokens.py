421b8659028d9102244df0033fe36dbdd44a1ead
Enzo POGGIO <poggio.enzo@gmail.com>
2019-07-15 17:43:55 +0200

    specific test

--- pyshell/main_tagstokens.py
+++ pyshell/main_tagstokens.py
@@ -5,7 +5,8 @@ from utils.tagstokens import TagsTokens
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        ENTRY = "ls -l; echo 'lol' 1;$(echo ls) \\\ncat ../*"
+        # ENTRY = "ls -l; echo 'lol' 1;$(echo ls) \\\ncat ../*"
+        ENTRY = "echo $PATH\\ "
     else:
         ENTRY = " ".join(sys.argv[1:])
     TAGSTOKENS = TagsTokens().init_with_input(ENTRY).check_syntax()
