c69d6463892a64fe8723b1ee8548450d8e432670
AbcSxyZ <rossi.sim@outlook.com>
2019-07-20 20:37:24 +0200

    add todo

--- pyshell/utils/execute.py
+++ pyshell/utils/execute.py
@@ -19,6 +19,7 @@ from utils.global_var import dprint
 
 #To do:
 # - Check where I'm loosing time with multiple CMDSUBST
+# - Check escapement (check Enzo who wanted to change tag for variables)
 
 def timer(function):
     def time_wrapper(*args, **kwargs):
