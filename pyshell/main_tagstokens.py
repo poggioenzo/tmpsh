28d7e743cabb38096157a9a603bca8f321b26b3c
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-12 15:55:03 +0200

    new test for heredocs

--- pyshell/main_tagstokens.py
+++ pyshell/main_tagstokens.py
@@ -7,7 +7,17 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         # ENTRY = "ls -l; echo 'lol' 1;$(echo ls) \\\ncat ../*"
         # ENTRY = "echo $PATH\\ "
-        ENTRY = '" $()"  '
+        ENTRY = ''' ls
+<< EOF << EOF
+'quote'
+$('quote " ')
+EOF
+"dquote" $("$(dquote)"; (lol)) stmt
+EOF
+<< EOF << EOF
+EOF
+EOF
+'''
     else:
         ENTRY = " ".join(sys.argv[1:])
     TAGSTOKENS = TagsTokens().init_with_input(ENTRY).check_syntax()
