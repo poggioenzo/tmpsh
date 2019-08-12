eaad4fb549220ce745efde1cfb7da06cd088039d
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-12 18:21:33 +0200

    test for heredocs

--- pyshell/main_tagstokens.py
+++ pyshell/main_tagstokens.py
@@ -6,18 +6,37 @@ from utils.tagstokens import TagsTokens
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         # ENTRY = "ls -l; echo 'lol' 1;$(echo ls) \\\ncat ../*"
-        # ENTRY = "echo $PATH\\ "
+        ENTRY = "echo $PATH\\ "
         ENTRY = ''' ls
 << EOF << EOF
 'quote'
 $('quote " ')
 EOF
-"dquote" $("$(dquote)"; (lol)) stmt
+
+
+
+
+
+    lol
+
+
+    "dquote" $("$(dquote)"; (lol)) stmt
 EOF
-<< EOF << EOF
-EOF
-EOF
+<<- $(  LOL  ) <<- EOF
+
+
+\t\t
+     lol
+
+
+
+        $(  LOL  )
+        python3 lol.py $(
+    EOF
+global_var
 '''
+        # ENTRY = '<<-EOF\n\t lol\n\t\tlol\nEOF'
+        # print(ENTRY)
     else:
         ENTRY = " ".join(sys.argv[1:])
     TAGSTOKENS = TagsTokens().init_with_input(ENTRY).check_syntax()
