b82ffd061b11994bd95bf6c89f02dfc468ad6d2a
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-14 11:26:09 +0200

    comment for test

--- src/ast_build/tokenizer.c
+++ src/ast_build/tokenizer.c
@@ -23,34 +23,32 @@ int ops_begin_with(char *pattern, t_pylst *leaf_op)
   return (FALSE);
 }
 
-char *add_token(char *current, t_pylst *tokens)
-{
-  if (current)
-  {
-    push_pylst(&tokens, current, ft_strlen(current), _chare);
-    ft_bzero(current, ft_strlen(current));
-  }
-  return current;
-}
-
-int span(char *command,int length_cmd,t_pylst *tokens,char *current)
-{
-  int j;
-  char *to_add;
-
-  j = 1;
-  current = add_token(current,tokens);
-  while (j <= g_grammar->maxlen_leaf_op
-        && j < length_cmd
-        && ops_begin_with((to_add = ft_strndup(command, j)), g_grammar-leaf_op)
-        && !(ft_strdel(&to_add)))
-    j += 1;
-
-
-}
-
-
-
+// char *add_token(char *current, t_pylst *tokens)
+// {
+//   if (current)
+//   {
+//     push_pylst(&tokens, current, ft_strlen(current), _chare);
+//     ft_bzero(current, ft_strlen(current));
+//   }
+//   return current;
+// }
+//
+// int span(char *command,int length_cmd,t_pylst *tokens,char *current)
+// {
+//   int j;
+//   char *to_add;
+//
+//   j = 1;
+//   current = add_token(current,tokens);
+//   while (j <= g_grammar->maxlen_leaf_op
+//         && j < length_cmd
+//         && ops_begin_with((to_add = ft_strndup(command, j)), g_grammar->leaf_op)
+//         && !(ft_strdel(&to_add)))
+//     j += 1;
+//
+//
+// }
+//
 
 
 
@@ -66,12 +64,14 @@ void tokenize(char *command, t_pylst *tokens)
   current = ft_strnew(length_cmd);
   while (i< length_cmd)
   {
-    if (ops_begin_with(&command[i], g_grammar->leaf_op))
-    {
-      // current = add_token(current,tokens);
-      // i += span(command, i, tokens, current);
 
-    }
+    ft_printf("%d\n",ops_begin_with(ft_strndup(command + i, 1), g_grammar->leaf_op));
+    // if (ops_begin_with(&command[i], g_grammar->leaf_op))
+    // {
+    //   // current = add_token(current,tokens);
+    //   // i += span(command, i, tokens, current);
+    //
+    // }
 
   }
   ft_strdel(&current);
