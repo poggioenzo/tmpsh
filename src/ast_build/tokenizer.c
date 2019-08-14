f9534bb8941250522b7a52d00cd422858ca782c1
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-14 17:42:52 +0200

    update still bus error

--- src/ast_build/tokenizer.c
+++ src/ast_build/tokenizer.c
@@ -11,69 +11,95 @@ void print_pylst(t_pylst *pylst)
     ft_printf("%s\n", value);
 }
 
+int in_pylst_chare(t_pylst *pylst, char *str)
+{
+  char  *value;
+
+  while (pylst_iter(pylst,(void **) &value))
+  {
+    if (value && !ft_strcmp(str, value))
+      return (TRUE);
+  }
+  return (FALSE);
+}
+
 int ops_begin_with(char *pattern, t_pylst *leaf_op)
 {
   int i;
-  char  *value;
+  char  *leaf;
+
 
   i = ft_strlen(pattern);
-  while (pylst_iter(leaf_op,(void **) &value))
-    if (ft_strncmp(pattern, value, i))
+  while (pylst_iter(leaf_op,(void **) &leaf))
+    if (!ft_strncmp(pattern, leaf, i))
       return (TRUE);
   return (FALSE);
 }
 
-// char *add_token(char *current, t_pylst *tokens)
-// {
-//   if (current)
-//   {
-//     push_pylst(&tokens, current, ft_strlen(current), _chare);
-//     ft_bzero(current, ft_strlen(current));
-//   }
-//   return current;
-// }
-//
-// int span(char *command,int length_cmd,t_pylst *tokens,char *current)
-// {
-//   int j;
-//   char *to_add;
-//
-//   j = 1;
-//   current = add_token(current,tokens);
-//   while (j <= g_grammar->maxlen_leaf_op
-//         && j < length_cmd
-//         && ops_begin_with((to_add = ft_strndup(command, j)), g_grammar->leaf_op)
-//         && !(ft_strdel(&to_add)))
-//     j += 1;
-//
-//
-// }
-//
+void *add_token(char *current, t_pylst *tokens)
+{
+  if (*current)
+  {
+    push_pylst(&tokens, current, ft_strlen(current) + 1, _chare);
+    ft_bzero(current, ft_strlen(current));
+  }
+}
+
+int span(char *command,t_pylst *tokens,char *current)
+{
+  int length_cmd;
+  int j;
+  char *to_add;
+
+  j = 1;
+  length_cmd = ft_strlen(command);
+  current = add_token(current,tokens);
+  ft_printf("length_cmd %d\n", length_cmd);
+  while (j <= g_grammar->maxlen_leaf_op
+        && j < length_cmd
+        && ops_begin_with((to_add = ft_strndup(command, j)), g_grammar->leaf_op)
+        && !(ft_strdel(&to_add)))
+  {
+    j += 1;
+  }
+  to_add = ft_strndup(command, j);
+  ft_printf("|%s|\n", to_add);
+  if (in_pylst_chare(g_grammar->leaf_op, to_add))
+  {
+    ft_printf( "YOLO\n" );
+    push_pylst(&tokens, to_add, ft_strlen(to_add) + 1, _chare);
+    ft_printf( "YOLO\n");
+  }
+  else
+    current = ft_strncat(current, command, 1);
+  ft_printf("j %d\n", j);
+  return (j);
 
 
+}
 
 
 void tokenize(char *command, t_pylst *tokens)
 {
-  int i;
-  int length_cmd;
   char *current;
+  char *tmp;
 
-  i = 0;
-  length_cmd = ft_strlen(command);
-  current = ft_strnew(length_cmd);
-  while (i< length_cmd)
+
+  current = ft_strnew(ft_strlen(command));
+  tokens = NULL;
+  while (*command)
   {
-
-    ft_printf("%d\n",ops_begin_with(ft_strndup(command + i, 1), g_grammar->leaf_op));
-    // if (ops_begin_with(&command[i], g_grammar->leaf_op))
-    // {
-    //   // current = add_token(current,tokens);
-    //   // i += span(command, i, tokens, current);
-    //
-    // }
-
+    ft_printf("-> %c <- \n", *command);
+    if (ops_begin_with((tmp = ft_strndup(command, 1)), g_grammar->leaf_op)
+        && !ft_strdel(&tmp))
+    {
+      ft_printf("SPAN\n");
+      command += span(command, tokens, current);
+    }
+    else
+      command++;
   }
+  print_pylst(tokens);
   ft_strdel(&current);
 
 }
