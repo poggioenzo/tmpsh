20d9177b668e47545aa92721ac6de2eb54c6cdb4
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-16 10:58:25 +0200

    norm_comment

--- src/ast_build/tokenizer/tokenizer.c
+++ src/ast_build/tokenizer/tokenizer.c
@@ -1,94 +1,166 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   tokenizer.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: epoggio <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/08/16 10:39:37 by epoggio      #+#   ##    ##    #+#       */
+/*   Updated: 2019/08/16 10:57:40 by epoggio     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include "tmpsh.h"
 #include "libft.h"
 #include "globals.h"
 #include "tokenizer_utils.h"
 
-static void add_token(char *current, t_pylst **tokens)
+/*
+** add_token:
+**
+** @current: actual created token.
+** @tokens: pylst of actual tokens.
+**
+** If current tokens is not empty it appends to tokens then reset.
+*/
+
+static	void	add_token(char *current, t_pylst **tokens)
 {
-  if (*current)
-  {
-    push_pylst(tokens, current, sizeof(char) * (ft_strlen(current) + 1), _chare);
-    ft_bzero(current, ft_strlen(current));
-  }
+	if (*current)
+	{
+		push_pylst(tokens, current, sizeof(char) *\
+			(ft_strlen(current) + 1), _chare);
+		ft_bzero(current, ft_strlen(current));
+	}
 }
 
-static int span(char *command,t_pylst **tokens,char *current)
-{
-  int length_cmd;
-  int j;
-  int i;
-  char *to_add;
+/*
+** span:
+**
+** @command: raw input shell command.
+** @tokens: pylst of actual tokens.
+** @current: actual created token.
+**
+** Append current token if exist.
+** If command begin with an operator it appends to tokens.
+** Then length of this operator is returned.
+** If not the first char of command it appends to current.
+** Then one is resturned.
+*/
 
-  j = 1;
-  i = 1;
-  length_cmd = ft_strlen(command);
-  add_token(current,tokens);
-  while (j <= g_grammar->maxlen_leaf_op
-        && j < length_cmd
-        && ops_begin_with((to_add = ft_strndup(command, j)),\
-                                                          g_grammar->leaf_op)
-        && !(ft_strdel(&to_add)))
-    j += 1;
-  to_add = ft_strndup(command, --j);
-  if (in_pylst_chare(to_add, g_grammar->leaf_op))
-  {
-    push_pylst(tokens, to_add, sizeof(char) * (ft_strlen(to_add) + 1), _chare);
-    i = j;
-  }
-  else
-    current[ft_strlen(current)] = *command;
-  ft_strdel(&to_add);
-  return (i);
+static	int		span(char *command, t_pylst **tokens, char *current)
+{
+	int		length_cmd;
+	int		j;
+	int		i;
+	char	*to_add;
+
+	j = 1;
+	i = 1;
+	length_cmd = ft_strlen(command);
+	add_token(current, tokens);
+	while (j <= g_grammar->maxlen_leaf_op
+			&& j < length_cmd
+			&& ops_begin_with((to_add = ft_strndup(command, j)),\
+				g_grammar->leaf_op)
+			&& !(ft_strdel(&to_add)))
+		j += 1;
+	to_add = ft_strndup(command, --j);
+	if (in_pylst_chare(to_add, g_grammar->leaf_op))
+	{
+		push_pylst(tokens, to_add, sizeof(char) *\
+				(ft_strlen(to_add) + 1), _chare);
+		i = j;
+	}
+	else
+		current[ft_strlen(current)] = *command;
+	return (ft_strdel_out(&to_add, i));
 }
 
-static char *escape_token(char *command, t_pylst **tokens, char *current)
+/*
+** escape_token:
+**
+** @command: raw input shell command.
+** @tokens: pylst of actual tokens.
+** @current: actual created token.
+**
+** Append current token if exist.
+** Append a token formed with a \ and the next char if its exists.
+** Return the incremented command pointer.
+*/
+
+static	char	*escape_token(char *command, t_pylst **tokens, char *current)
 {
-  add_token(current,tokens);
-  command++;
-  current[ft_strlen(current)] = g_grammar->escape[0];
-  current[ft_strlen(current)] = *command;
-  add_token(current, tokens);
-  command++;
-  return (command);
+	add_token(current, tokens);
+	command++;
+	current[ft_strlen(current)] = g_grammar->escape[0];
+	if (*command)
+	{
+		current[ft_strlen(current)] = *command;
+		command++;
+	}
+	add_token(current, tokens);
+	return (command);
 }
 
-static int span_space(char *command, t_pylst **tokens, char *current)
-{
-  int i;
-  char *spaces;
+/*
+** span_space:
+**
+** @command: raw input shell command.
+** @tokens: pylst of actual tokens.
+** @current: actual created token.
+**
+** Append current token if exist.
+** Then span all kind of spaces and append them to tokens.
+** Return the number of spanned spaces.
+*/
 
-  i = 1;
-  add_token(current, tokens);
-  while(ft_incharset(command[i], " \t"))
-    i++;
-  spaces = ft_strndup(command, i);
-  push_pylst(tokens, spaces, sizeof(char) * (ft_strlen(spaces) + 1), _chare);
-  ft_strdel(&spaces);
-  return (i);
+static	int		span_space(char *command, t_pylst **tokens, char *current)
+{
+	int		i;
+	char	*spaces;
+
+	i = 1;
+	add_token(current, tokens);
+	while (ft_incharset(command[i], " \t"))
+		i++;
+	spaces = ft_strndup(command, i);
+	push_pylst(tokens, spaces, sizeof(char) * (ft_strlen(spaces) + 1), _chare);
+	ft_strdel(&spaces);
+	return (i);
 }
 
+/*
+** tokenize:
+**
+** @command: raw input shell command.
+** @tokens: address of NULL pointer of t_pylst tokens.
+**
+** Tokenize input in function of g_grammar.
+** Put each tokens in *tokens.
+*/
 
-void tokenize(char *command, t_pylst **tokens)
+void			tokenize(char *command, t_pylst **tokens)
 {
-  char *current;
-  char *tmp;
+	char *current;
+	char *tmp;
 
-  current = ft_strnew(ft_strlen(command));
-  while (*command)
-  {
-    if (ops_begin_with((tmp = ft_strndup(command, 1)), g_grammar->leaf_op)
-        && !ft_strdel(&tmp))
-      command += span(command, tokens, current);
-    else if (*command == g_grammar->escape[0])
-      command = escape_token(command, tokens, current);
-    else if (ft_incharset(*command, " \t"))
-      command += span_space(command, tokens, current);
-    else
-    {
-      current[ft_strlen(current)] = *command;
-      command++;
-    }
-  }
-  print_pylst_chare(*tokens);
+	current = ft_strnew(ft_strlen(command));
+	while (*command)
+	{
+		if (ops_begin_with((tmp = ft_strndup(command, 1)), g_grammar->leaf_op)
+				&& !ft_strdel(&tmp))
+			command += span(command, tokens, current);
+		else if (*command == g_grammar->escape[0])
+			command = escape_token(command, tokens, current);
+		else if (ft_incharset(*command, " \t"))
+			command += span_space(command, tokens, current);
+		else
+		{
+			current[ft_strlen(current)] = *command;
+			command++;
+		}
+	}
 }
--- src/ast_build/tokenizer/tokenizer_utils.c
+++ src/ast_build/tokenizer/tokenizer_utils.c
@@ -1,20 +1,43 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   tokenizer_utils.c                                .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: epoggio <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/08/16 10:04:32 by epoggio      #+#   ##    ##    #+#       */
+/*   Updated: 2019/08/16 10:06:53 by epoggio     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
 #include "tmpsh.h"
 #include "libft.h"
 
-int ops_begin_with(char *pattern, t_pylst *leaf_op)
-{
-  int     i;
-  char    *leaf;
-  t_bool  ret;
+/*
+** ops_begin_with:
+**
+** @pattern: operator tested pattern.
+** @leaf_op: list of atomic operator of the grammar.
+**
+** Return True if the lengthed begin of the given pattern is identical to
+** the lengthed begin of one operator in leaf_op.
+*/
 
-  ret = FALSE;
-  i = ft_strlen(pattern);
-  while (pylst_iter(leaf_op,(void **) &leaf))
-    if (ft_strnequ(pattern, leaf, i))
-    {
-      ret = TRUE;
-      leaf_op->iter_item = NULL;
-      break ;
-    }
-  return (ret);
+int	ops_begin_with(char *pattern, t_pylst *leaf_op)
+{
+	int		i;
+	char	*leaf;
+	t_bool	ret;
+
+	ret = FALSE;
+	i = ft_strlen(pattern);
+	while (pylst_iter(leaf_op, (void **)&leaf))
+		if (ft_strnequ(pattern, leaf, i))
+		{
+			ret = TRUE;
+			leaf_op->iter_item = NULL;
+			break ;
+		}
+	return (ret);
 }
