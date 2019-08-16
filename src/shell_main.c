f7a27a7162078c3ed178f1bd9686ec9e974a5b94
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-16 16:44:35 +0200

    new test

--- src/shell_main.c
+++ src/shell_main.c
@@ -5,27 +5,35 @@
 #include <fcntl.h>
 
 #include "tokenizer.h"
+#include "tagstokens.h"
+#include "str_tagstokens.h"
 #include "grammar_init.h"
 
 #include "libft.h"
 #include "builtins.h"
 
-int		main(int argc, char **argv, char **environ)
+int		main()//int argc, char **argv, char **environ)
 {
-	int		status = 0;
+	//int		status = 0;
 	char	*grammar_abs;
 	char	*grammar_file = "/pyshell/grammar/grammar.txt";
 	t_pylst *tokens;
+	t_tagstokens *tgtk;
 
 
 	tokens = NULL;
+	tgtk = NULL;
 	setup_freefct();
 	grammar_abs = getcwd(NULL, 0);
 	grammar_abs = ft_fstrjoin(&grammar_abs, &grammar_file, true, false);
 	grammar_init(grammar_abs);
 	FREE(grammar_abs);
-	ft_printf("QWERTYUI\n");
-	tokenize("$$ text $# text $* text $@ ", &tokens);
+	// ft_printf("QWERTYUI\n");
+	tokenize("$$ test << $@ ", &tokens);
+	print_pylst_chare(tokens);
+	init_tagstokens(&tgtk, tokens, NULL);
+	get_tag_tagstokens(tgtk);
+	print_tagstokens(tgtk);
 	//setup_variables_elements(environ);
 
 	/*
