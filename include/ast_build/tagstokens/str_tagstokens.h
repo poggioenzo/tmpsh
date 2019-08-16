1e058eefcc947022b24bcb3e26e3d32fced1cea0
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-16 16:47:05 +0200

    create new fontion for print and change to string tagstokens

--- include/ast_build/tagstokens/str_tagstokens.h
+++ include/ast_build/tagstokens/str_tagstokens.h
@@ -2,5 +2,6 @@
 # define STR_TAGSTOKENS_H
 
 char *str_tagstokens(t_tagstokens *self);
+void print_tagstokens(t_tagstokens *self);
 
 #endif
--- src/ast_build/tagstokens/str_tagstokens.c
+++ src/ast_build/tagstokens/str_tagstokens.c
@@ -2,18 +2,88 @@
 #include "tmpsh.h"
 #include "libft.h"
 #include "globals.h"
-#include "tokenizer_utils.h"
-#include "tokenizer.h"
-
-static int get_length(t_tagstokens *self)
-{}
+#include "iter_tagstokens.h"
 
 
+
+
+// char *str_chare_pylst(t_pylst pylst)
+// {
+//   int sum;
+//
+//   sum = 0;
+//
+// }
+
+
+static int str_get_length(t_tagstokens *self, int *spacing)
+{
+  int ret;
+  int len_tag;
+  int len_token;
+  char *tag;
+  char *token;
+
+
+  ret = 0;
+  while (iter_tagstokens(self, &token, &tag))
+  {
+    len_tag = ft_strlen(tag);
+    len_token = ft_strlen(token);
+    ret += (len_token>len_tag) ? len_token : len_tag;
+    spacing[self->iter] = ++ret;
+  }
+  return (ret);
+}
+
+static void fill_tags_tokens(t_tagstokens *self, char *str, int *spacing)
+{
+  char *tag;
+  char *token;
+  int i;
+  int remove;
+  int countdown;
+
+  i = 0;
+  remove = 0;
+  countdown = 2;
+  while (countdown--)
+  {
+      while (iter_tagstokens(self, &token, &tag))
+      {
+        while ((countdown ? *tag : *token))
+          str[i++] = (countdown ? *tag++ : *token++);
+        while (i - remove< spacing[self->iter])
+          i++;
+      }
+      i--;
+      str[i++] = '\n';
+      remove = i;
+  }
+  str[i] = 0;
+}
 
 char *str_tagstokens(t_tagstokens *self)
 {
   char *str;
+  char *tmp;
+  int  len;
+  int *spacing;
 
+  spacing = (int *)ft_memalloc(self->length * sizeof(int));
+  len = (str_get_length(self, spacing) + 1) * 2 + 1 ;
+  str = (char *)ft_memalloc(len);
+  ft_memset((void *)str, 32, len - 1);
+  fill_tags_tokens(self, str, spacing);
+  tmp = str;
 
-  return ("LOL");
+  return (str);
 }
+
+void print_tagstokens(t_tagstokens *self)
+{
+  ft_printf(str_tagstokens(self));
+}
+
+//\nValid: FALSE | Incomplete: FALSE | Token_error: "
+// | Length:
