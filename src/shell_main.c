7c7b7b2a4f5c99c272390e929d9fb2fdd2a8a66d
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-17 13:50:56 +0200

    new main for new test

--- src/shell_main.c
+++ src/shell_main.c
@@ -7,20 +7,24 @@
 #include "tokenizer.h"
 #include "tagstokens.h"
 #include "str_tagstokens.h"
+#include "tagging.h"
 #include "grammar_init.h"
 
 #include "libft.h"
 #include "builtins.h"
 
-int		main()//int argc, char **argv, char **environ)
+int		main(int argc, char **argv)//char **environ)
 {
-	//int		status = 0;
 	char	*grammar_abs;
 	char	*grammar_file = "/pyshell/grammar/grammar.txt";
 	t_pylst *tokens;
 	t_tagstokens *tgtk;
 
-
+	if (argc != 2)
+	{
+		ft_printf("Usage:"PURPLE" ./tmpsh 'command'\n"WHITE);
+		return (64);
+	}
 	tokens = NULL;
 	tgtk = NULL;
 	setup_freefct();
@@ -28,18 +32,9 @@ int		main()//int argc, char **argv, char **environ)
 	grammar_abs = ft_fstrjoin(&grammar_abs, &grammar_file, true, false);
 	grammar_init(grammar_abs);
 	FREE(grammar_abs);
-	// ft_printf("QWERTYUI\n");
-	tokenize("$$ test << $@ ", &tokens);
-	print_pylst_chare(tokens);
+	tokenize(argv[1], &tokens);
 	init_tagstokens(&tgtk, tokens, NULL);
 	get_tag_tagstokens(tgtk);
 	print_tagstokens(tgtk);
-	//setup_variables_elements(environ);
-
-	/*
-	fd_debug = open("/dev/ttys003",  O_RDWR | O_TRUNC | O_CREAT, 0777);
-	if (argc == 1)
-		return (prompt_loop());
-	return (status);
-	*/
+	return (0);
 }
