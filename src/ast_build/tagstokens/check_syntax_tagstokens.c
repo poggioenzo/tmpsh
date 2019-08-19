8d2294cb4117be32811c668b17444e370e9a0901
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-19 13:56:20 +0200

    wait for tagstokensmonitor and shiftreduce

--- src/ast_build/tagstokens/check_syntax_tagstokens.c
+++ src/ast_build/tagstokens/check_syntax_tagstokens.c
@@ -22,14 +22,16 @@
 
 
 
+
+
 void check_syntax_tagstokens(t_tagstokens *self)
 {
     // init_tagstokensmonitor(self);
-    if (self->valid)
-    {
-        // self->stack = tgtk_shiftreduce(self, g_grammar->grammar);
-        if (self->length > 0 && )
-
-    }
-    clear_stack_tagstokens(self);
+    // if (self->valid)
+    // {
+    //     // self->stack = tgtk_shiftreduce(self, g_grammar->grammar);
+    //     if (self->length > 0 && )
+    //
+    // }
+    // clear_stack_tagstokens(self);
 }
