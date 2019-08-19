e944b1f7961ee4accbaf7d5f60a1b56ec0f8310e
AbcSxyZ <rossi.sim@outlook.com>
2019-08-19 09:20:17 +0200

    start translate background builtins

---
+++ src/builtins/background.c
@@ -0,0 +1,17 @@
+#include "tmpsh.h"
+#include "libft.h"
+
+int		built_jobs(char **argv, char **environ)
+{
+	t_pylst		*job;
+	t_acb		*command_branch;
+
+	while (iter_pylst(g_jobs->list_jobs, (void **)&jobs))
+	{
+		command_branch = (t_acb *)index_pylst(jobs, -1)->value;
+		ft_printf("[%d] : %s\n", command_branch->pid, command_branch->command);
+	}
+	return (0);
+}
+
+int		
