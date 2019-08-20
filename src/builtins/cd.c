bfc523ece2072abc2850a2ebd2c922c7615c4681
AbcSxyZ <rossi.sim@outlook.com>
2019-08-20 10:18:15 +0200

    start rework of cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -10,6 +10,8 @@ static char		*get_oldpwd(void)
 	return (oldpwd);
 }
 
+
+
 int			built_cd(char **args, char **environ)
 {
 	char	*direction;
@@ -36,4 +38,53 @@ int			built_cd(char **args, char **environ)
 	ft_setenv("OLDPWD", oldpwd);
 	ft_strdel(&oldpwd);
 	return (0);
+}
+
+static int		check_rights(char *filename)
+{
+	if (access(filename, F_OK) == -1)
+		return (0);
+	if (access(filename, X_OK) == -1)
+		return (0);
+}
+
+static char		*parse_cdpath(char *pathname)
+{
+	char	*cdpath;
+	char	**split_folders;
+	int		index;
+	char	*new_dir;
+
+	cdpath = ft_getenv("CDPATH");
+	if (!cdpath)
+		return (pathname);
+	split_folders = ft_strsplit(cdpath, ":");
+	index = 0;
+	while (split_folders[index])
+	{
+		new_dir = ft_filejoin
+	}
+}
+
+int		built_cd(char **argv, char **environ)
+{
+	t_pylst		*options;
+	int			argc;
+	char		*new_dir;
+
+	options = argparser(argv);
+	argc = ft_arraylen(argv);
+	if (argc == 0 && !(new_dir = ft_getenv("HOME")))
+		return (free_pylst(&options, 0));
+	if (argc >= 2)
+	{
+		ft_printf("cd: too much arguments\n");
+		return (free_pylst(&options, 1));
+	}
+	if (argv[0][0] == '/')
+		new_dir = argv[0];
+	else if (ft_start_with(argv[0], ".") || ft_start_with(argv[0], ".."))
+		new_dir = parse_cdpath(argv[0]);
+	else
+		new_dir = argv[0];
 }
