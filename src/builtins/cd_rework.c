3bf6ea86694d6beae06706b9e14650da2d00eb72
AbcSxyZ <rossi.sim@outlook.com>
2019-08-21 02:11:46 +0200

    correct canonical conversion

--- src/builtins/cd_rework.c
+++ src/builtins/cd_rework.c
@@ -23,16 +23,6 @@ int			next_fileis(char *abspath, char *expected_name)
 	return (0);
 }
 
-int			file_len(char *path)
-{
-	int		index;
-
-	index = 0;
-	while (path[index] && path[index] != '/')
-		index++;
-	return (index);
-}
-
 static void		remove_double_slash(char *path)
 {
 	char	*slash;
@@ -58,53 +48,9 @@ void		retrieve_prev(char *absfile, char *curr_file, char **prev_file)
 	*prev_file = NULL;
 	while (finder != curr_file)
 	{
-		printf("FINDER : %s CURR %S PREV %s\n", finder, curr_file, prev_file);
 		*prev_file = finder;
 		finder = ft_strchr(finder + 1, '/');
 	}
-	printf("PREV : %s ---------\n", *prev_file);
-}
-
-int			iter_filecompo(char *absfile, char **prev, char **curr)
-{
-	static char		*curr_save = NULL;
-
-	printf("IN iter save %s : prev %s | curr %s \n",
-			curr_save, *prev, *curr);
-	if (!curr_save)
-	{
-		curr_save = absfile;
-		*curr = curr_save;
-		*prev = NULL;
-		return (1);
-	}
-	else if (!*curr_save)
-	{
-		curr_save = NULL;
-		return (0);
-	}
-	if (curr_save != *curr)
-	{
-		curr_save = *curr;
-		if (!*curr_save)
-			return (0);
-		printf("pos %s in %s\n", curr_save, absfile);
-		retrieve_prev(absfile, curr_save, prev);
-		return (1);
-	}
-	else
-		*prev = curr_save;
-	curr_save = ft_strchr(curr_save + 1, '/');
-	if (curr_save && curr_save[1])
-	{
-		*curr = curr_save;
-		printf("OUT iter save %s : prev %s | curr %s \n",
-				curr_save, *prev, *curr);
-		return (1);
-	}
-	printf("OUT prev %s curr %s\n", *prev, *curr);
-	curr_save += ft_strlen(curr_save);
-	return (1);
 }
 
 int		len_filename(char *filename)
@@ -117,39 +63,42 @@ int		len_filename(char *filename)
 	return (index);
 }
 
-void		remove_dot(char *relpath)
+void		remove_dot(char *absolute, char *position)
 {
 	int		len_dot;
-	int		end_len;
+	int		is_root;
+	char	*to_del;
 
-	len_dot = len_filename(relpath);
-	end_len = ft_strlen(relpath + len_dot) + 1;
-	ft_memmove(relpath, relpath + len_dot, end_len);
+	is_root = absolute == position && *absolute == '/';
+	len_dot = len_filename(position);
+	to_del = position + len_dot;
+	ft_strcpy(position, to_del);
+	if (is_root && ft_strlen(absolute) == 0)
+		ft_strcpy(absolute, "/");
 }
 
-void	reset_previous_dir(char **curr_file, char *prev_file)
+void	reset_previous_dir(char *abspath, char **curr_file)
 {
+	char		*prev_file;
 	char		*dot_end;
+	int			is_root;
 
-	printf("there prev : %s, curr : %s\n", prev_file, *curr_file);
+	retrieve_prev(abspath, *curr_file, &prev_file);
+	if (abspath == *curr_file && *abspath == '/')
+		prev_file = abspath;
+	is_root = prev_file == abspath && *abspath == '/';
 	if (!prev_file)
 		return ;
-	if (!next_fileis(prev_file, ".."))
+	if (!next_fileis(prev_file, "..") || is_root)
 	{
 		if (!(dot_end = ft_strchr((*curr_file) + 1 , '/')))
 		{
-			*prev_file = '\0';
-			*curr_file = prev_file;
-			printf("CUT\n");
+			prev_file += is_root;
+			prev_file[0] = '\0';
 		}
 		else
-		{
-			ft_printf("dot |%s|, prev_file |%s|\n", dot_end, prev_file);
 			ft_strcpy(prev_file, dot_end);
-			ft_printf("prevdel %s\n", prev_file);
-			*curr_file = prev_file;
-
-		}
+		*curr_file = prev_file;
 	}
 }
 
@@ -157,23 +106,19 @@ char		*canonicalize(char *path)
 {	
 	char *prev;
 	char *curr;
+	int	 is_root;
 
 	remove_double_slash(path);
 	prev = NULL;
-	curr = NULL;
-	while (iter_filecompo(path, &prev, &curr))
+	curr = path;
+	while (curr && *curr)
 	{
-		printf("FILE : %s, pos %s\n", path, curr);
 		if (next_fileis(curr, "."))
-		{
-			remove_dot(curr);
-			curr = prev;
-		}
+			remove_dot(path, curr);
 		else if (next_fileis(curr, ".."))
-		{
-			reset_previous_dir(&curr, prev);
-			ft_printf("WHEN REMOVED %s\n", path);
-		}
+			reset_previous_dir(path, &curr);
+		else
+			curr = ft_strchr(curr + 1, '/');
 	}
 }
 
