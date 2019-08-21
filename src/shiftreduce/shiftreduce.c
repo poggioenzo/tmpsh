995857d7daf57f6482c034fb7bed0975f3e2fed4
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-21 13:10:28 +0200

    rm commetn

--- src/shiftreduce/shiftreduce.c
+++ src/shiftreduce/shiftreduce.c
@@ -5,36 +5,6 @@
 # include "tokenizer.h"
 # include "tagstokens.h"
 
-
-// def check_forbidden(tags, grammar):
-//     i = 0
-//     len_tags = len(tags)
-//     instack = []
-//     key = ''
-//     spaces = 0
-//     while i < len_tags:
-//         key = ' '.join(instack)
-//         if key in grammar.reverse and grammar.reverse[key] == 'FORBIDDEN':
-//             if tags[i - len(instack)] == 'SPACES':
-//                 tags[i - len(instack) - 1] = 'FORBIDDEN'
-//             else:
-//                 tags[i - len(instack)] = 'FORBIDDEN'
-//             spaces = 0
-//             instack = []
-//             break
-//         elif tags[i] == 'SPACES':
-//             spaces += 1
-//         elif instack == [] or tk.ops_begin_with(
-//                 key, grammar.grammar['FORBIDDEN']):
-//             instack.append(tags[i])
-//         else:
-//             instack = []
-//         i += 1
-//     key = ' '.join(instack)
-//     if key in grammar.reverse and grammar.reverse[key] == 'FORBIDDEN':
-//         tags[i + spaces - len(instack)] = 'FORBIDDEN'
-//     return tags
-
 static t_bool cond_check_forbidden_shiftreduce(char *key, int i, t_pylst *instack, t_pylst *tags)
 {
     int index_forbidden;
@@ -90,6 +60,6 @@ void shiftreduce(t_tagstokens *tgtk)
     int forbidden;
     char *next_tag;
 
-    
+
 
 }
