0a17af0eb8ab454f0a4c70940efb7dea8483bda9
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-24 23:01:51 +0200

    remove useless print

--- src/ast_build/shiftreduce/reduce_all_shiftreduce.c
+++ src/ast_build/shiftreduce/reduce_all_shiftreduce.c
@@ -7,7 +7,6 @@ static t_pylst **reduce(t_pylst **stack, int instack)
 {
     char *key;
 
-    ft_printf(str_chare_pylst(*stack));
     key = join_pylst(index_pylst(*stack, instack), " ");
     del_portion_pylst(stack, instack, len_pylst(*stack));
     push_pylst(stack, search_value(g_grammar->reverse, key) , 0, _ptr);
@@ -18,7 +17,6 @@ static t_pylst **reduce(t_pylst **stack, int instack)
 
 t_pylst *reduce_all(t_pylst **stack, int instack, char *next_tag)
 {
-    ft_printf("################### REDUCE ###################\n" );
     while (instack > -1)
     {
         stack = reduce(stack, instack);
