b464ea5f48692eb777e44581a3401df871b29769
AbcSxyZ <rossi.sim@outlook.com>
2019-08-24 18:50:05 +0200

    work on builtins jobs && fg

--- pyshell/utils/builtins/background.py
+++ pyshell/utils/builtins/background.py
@@ -3,17 +3,61 @@ import os
 import sys
 import signal
 
+def is_job_id(argument_name):
+    return argument_name[0] == "%" and argument_name[1:].isdigit() 
+
+
+def diplay_job(job, index, p_flag, l_flag):
+    status = "Running" if job[0].running else "Stopped"
+    #print only PGID of the job.
+    if p_flag:
+        print(job[-1].pgid)
+    elif l_flag:
+        pgid = job[-1].pgid
+        print("[{}] - {} {}     {}".format(index, pgid, status, job[-1].command))
+    else:
+        print("[{}] - {}  {}".format(index, status, job[-1].command))
+
+
+
 def jobs(argv, environ):
-    for task in gv.JOBS:
-        print("{} : {}".format(task[-1].pid, task[-1].command))
+    argc = len(argv)
+    l_flag = False
+    p_flag = False
+    if argc >= 1 and argv[0] == "-p":
+        p_flag = True
+        argv.pop(0)
+    elif argc >= 1 and argv[0] == "-l":
+        l_flag = True
+        argv.pop(0)
+    if len(argv) > 0:
+        for argument in argv:
+            if is_job_id(argument):
+                index = int(argument[1:])
+                job = gv.JOBS.get_job(index)
+                if job == None:
+                    print("jobs: No such job '{}'".format(argument), file=sys.stderr)
+                    return 1
+                else:
+                    diplay_job(job, index, p_flag, l_flag)
+            else:
+                print("jobs: No such job '{}'".format(argument))
+                return 1
+
+    else:
+        for index, task in enumerate(gv.JOBS):
+            diplay_job(task, index, p_flag, l_flag)
+    return 0
 
 def fg(argv, environ):
     if len(argv) == 0:
-        index = 0
-    else:
-        index = int(argv)
-    if index >= len(gv.JOBS.list_jobs):
-        print("fg: job not found: {}".format(index), file=sys.stderr)
-        return 1
-    gv.JOBS.relaunch(index)
+        argv = ["%0"]
+    for argument in argv:
+        if is_job_id(argument):
+            index = int(argument[1:])
+            if gv.JOBS.get_job(index) is None:
+                print("jobs: No such job '{}'".format(argument), file=sys.stderr)
+                return 1
+            else:
+                gv.JOBS.relaunch(index)
     return 0
