5af78d9708c4b80dcaba0f4f5cf4f65966891774
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-27 15:53:20 +0200

    simplify str_command_tagstokens

--- src/ast_build/tagstokens/str_command_tagstokens.c
+++ src/ast_build/tagstokens/str_command_tagstokens.c
@@ -4,34 +4,23 @@
 
 char *str_command_tagstokens(t_tagstokens *self)
 {
-    size_t length;
-    size_t i;
     char *tok;
     char *tag;
     char *str;
 
-    length = 0;
-    print_tagstokens(self);
-    while (iter_tagstokens(self, &tok, &tag))
-        length += ft_strlen(tok);
-    str = ft_strnew(length);
-    // if (str = ft_strnew(length) && length == 0)
-    //     return (str);
-    length = 0;
+    str = ft_strnew(0);
     while (iter_tagstokens(self, &tok, &tag))
     {
-        if (!ft_strequ("SPACES", tag))
+        if (ft_strequ("SPACES", tag))
+            str = free_join(str, " ", FALSE);
+        else if (ft_strequ("SUBAST", tag))
         {
-            i = 0;
-            ft_printf("%p\n", tok);
-            while (tok[i])
-            {
-                str[length++] = tok[i++];
-                ft_printf("%s\n", str);
-            }
+            str = free_join(str, YELLOW, FALSE);
+            str = free_join(str, tok, FALSE);
+            str = free_join(str, WHITE, FALSE);
         }
         else
-            str[length++] = ' ';
+            str = free_join(str, tok, FALSE);
     }
     return(str);
 }
