cd3f209c8789bfa3f02adfce9eef13b5830bfaec
AbcSxyZ <rossi.sim@outlook.com>
2019-08-27 14:09:33 +0200

    add reverse function

--- Libft/pylst/python_list.c
+++ Libft/pylst/python_list.c
@@ -573,3 +573,23 @@ void		sort_pylst(t_pylst **pylst, int (*cmp)(t_pylst *, t_pylst *))
 	}
 	*pylst = sorted_pylst;
 }
+
+void	reverse_pylst(t_pylst **pylst)
+{
+	t_pylst		*reverse_lst;
+	t_pylst		*precedence;
+	t_pylst		*next;
+
+	if (!*pylst)
+		return ;
+	precedence = NULL;
+	reverse_lst = *pylst;
+	while (reverse_lst)
+	{
+		next = reverse_lst->next;
+		reverse_lst->next = precedence;
+		precedence = reverse_lst;
+		reverse_lst = next;
+	}
+	*pylst = reverse_lst;
+}
