3e0f0396c4a3bb5163f1b1990993d4331ffec177
AbcSxyZ <rossi.sim@outlook.com>
2019-08-27 19:43:59 +0200

    add in.c function

--- Libft/libft.h
+++ Libft/libft.h
@@ -132,6 +132,7 @@ char			*ft_strdup(const char *src);
 char			**ft_duparray(char **array);
 char			**ft_array_extend(char **array, char *string, int dup_str);
 int				ft_array_in(char **array, char *string);
+int				in(char *value, ...);
 char			*ft_strcat(char *dest, const char *src);
 char			*ft_strncat(char *s1, const char *s2, size_t n);
 size_t			ft_strlcat(char *dst, const char *src, size_t size);
