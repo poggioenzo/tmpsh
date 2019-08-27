75e040ec3a74017ff31d2ec81b25226265ba0700
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-27 18:43:23 +0200

    add interface for future methode

---
+++ include/ast_build/redirectionfd/get_heredoc_redfd.h
@@ -0,0 +1,6 @@
+#ifndef GET_HEREDOC_REDFD_H
+# define GET_HEREDOC_REDFD_H
+
+int get_heredoc_redfd(void);
+
+#endif
---
+++ src/ast_build/redirectionfd/get_heredoc_redfd.c
@@ -0,0 +1,18 @@
+#include "get_heredoc_redfd.h"
+
+
+/*
+** get_heredoc_redfd:
+**
+** description:
+** This actual redirection is an Heredoc. This function affect the good pointer
+** of the tagstokens containing the heredoc.
+**
+** parameter:
+** - (t_redirection_fd) self : the actual redirection structure.
+*/
+
+int get_heredoc_redfd(void)
+{
+	return (0);
+}
