4c8b548af7eb0248ff9cef26b88657cec163b090
AbcSxyZ <rossi.sim@outlook.com>
2019-08-30 17:35:18 +0200

    allow space in assignation

--- src/execution/assignation.c
+++ src/execution/assignation.c
@@ -32,6 +32,33 @@ static void		fill_assignation(t_pylst **assignations, char *key, \
 }
 
 /*
+** count_space:
+**
+** Count the number of spaces containing between assignation tokens,
+** if there is a SPACES tokens at the left or the right of
+** the ASSIGNATION_EQUAL or CONCATENATION element.
+*/
+
+static int		count_space(t_tagstokens *tagstoks)
+{
+	int			index;
+	int			nbr_spaces;
+	char		*tag;
+
+	index = 0;
+	nbr_spaces = 0;
+	while (index < 3)
+	{
+		tag = vindex_pylst(tagstoks->tags, index + nbr_spaces);
+		if (ft_strequ(tag, "SPACES"))
+			nbr_spaces++;
+		else
+			index++;
+	}
+	return (nbr_spaces);
+}
+
+/*
 ** delete_variables:
 **
 ** @tagstok: tagstokens where the assignation tag/tokens have to be removed.
@@ -45,13 +72,21 @@ static void		fill_assignation(t_pylst **assignations, char *key, \
 static void		delete_variables(t_tagstokens *tagstok, t_pylst *assignations)
 {
 	int		nbr_assignations;
+	int		nbr_spaces_token;
 
 	nbr_assignations = len_pylst(assignations);
-	if (nbr_assignations > 0)
+	while (nbr_assignations--)
 	{
-		del_portion_pylst(&tagstok->tags, 0, nbr_assignations * 3);
-		del_portion_pylst(&tagstok->tokens, 0, nbr_assignations * 3);
+		nbr_spaces_token = count_space(tagstok);
+		del_portion_pylst(&tagstok->tags, 0, 3 + nbr_spaces_token);
+		del_portion_pylst(&tagstok->tokens, 0, 3 + nbr_spaces_token);
 		update_length_tagstokens(tagstok);
+		if (tagstok->length > 0 && ft_strequ(tagstok->tags->value, "SPACES"))
+		{
+			del_portion_pylst(&tagstok->tags, 0, 1);
+			del_portion_pylst(&tagstok->tokens, 0, 1);
+			update_length_tagstokens(tagstok);
+		}
 	}
 }
 
@@ -74,6 +109,7 @@ t_pylst			*retrieve_assignation(t_acb *branch)
 	char			*last_stmt;
 	size_t			index;
 	char			*tag;
+	char			*next_tag;
 
 	index = 0;
 	assignation_list = NULL;
@@ -85,7 +121,8 @@ t_pylst			*retrieve_assignation(t_acb *branch)
 			last_stmt = vindex_pylst(branch->tagstokens->tokens, index);
 		else if (in(tag, "CONCATENATION", "ASSIGNATION_EQUAL", NULL))
 		{
-			index += 1;
+			next_tag = vindex_pylst(branch->tagstokens->tags, index + 1);
+			index += 1 + ft_strequ(next_tag, "SPACES");
 			fill_assignation(&assignation_list, last_stmt, tag,\
 				vindex_pylst(branch->tagstokens->tokens, index));
 			last_stmt = NULL;
