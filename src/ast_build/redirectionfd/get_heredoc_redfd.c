62797d1ac344f5c4a02c4eeccbb0c71625da7bba
AbcSxyZ <rossi.sim@outlook.com>
2019-08-31 16:16:05 +0200

    add get_heredoc_redfd function

--- src/ast_build/redirectionfd/get_heredoc_redfd.c
+++ src/ast_build/redirectionfd/get_heredoc_redfd.c
@@ -24,7 +24,15 @@
 ** - (t_redirection_fd) self : the actual redirection structure.
 */
 
-int	get_heredoc_redfd(void)
+int	get_heredoc_redfd(t_redirection_fd *self)
 {
+	t_tagstokens	*tagstokens;
+
+	if (len_pylst(g_heredocs) > 0)
+	{
+		tagstokens = ((t_heredocs*)vindex_pylst(g_heredocs, -1))->tagstokens;
+		pop_pylst(g_heredocs, -1);
+		self->heredoc_ast = init_ast(tagstokens);
+	}
 	return (0);
 }
