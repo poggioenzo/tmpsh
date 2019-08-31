32ab4527c8dfc888d09168217e3d0f526c17ad15
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-31 16:44:53 +0200

    add all needed header

--- include/ast_build/tagstokensmonitor/ttm.h
+++ include/ast_build/tagstokensmonitor/ttm.h
@@ -24,5 +24,18 @@
 
 # include "init_ttm.h"
 # include "reset_ttm.h"
+# include "check_alias_ttm.h"
+# include "check_ttm.h"
+# include "in_command_sh_ttm.h"
+# include "is_abs_terminator_ttm.h"
+# include "is_braceparam_ttm.h"
+# include "is_heredocs_ttm.h"
+# include "is_newline_ttm.h"
+# include "is_quote_ttm.h"
+# include "is_redirection_ttm.h"
+# include "is_sub_process_ttm.h"
+# include "next_ttm.h"
+# include "op_selector_ttm.h"
+# include "check_alias_ttm.h"
 
 #endif
