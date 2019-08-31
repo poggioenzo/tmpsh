f3224f0f060f2f368a96476baa4a9190d8c1396c
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-31 18:05:08 +0200

    add free methode

---
+++ include/ast_build/heredocs/free_heredocs.h
@@ -0,0 +1,11 @@
+#ifndef FREE_HEREDOCS_H
+# define FREE_HEREDOCS_H
+
+/*
+** The last line of this file should be "#endif".
+** If not update header by project_manager.py (pm) fails.
+*/
+
+int free_heredocs(void);
+
+#endif
---
+++ src/ast_build/heredocs/free_heredocs.c
@@ -0,0 +1,20 @@
+#include "free_heredocs.h"
+
+
+/*
+** free_heredocs:
+**
+** description:
+** describe what you function do.
+**
+** parameter:
+** - (type) name : what is this param?
+**
+** return value:
+** - (type) value : descibe output.
+*/
+
+int free_heredocs(void)
+{
+	return (0);
+}
