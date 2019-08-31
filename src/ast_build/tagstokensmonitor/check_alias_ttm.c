6aa5c8c60c704abd0887d56c6c581aa042230979
Enzo POGGIO <poggio.enzo@gmail.com>
2019-08-31 18:02:21 +0200

    rename func and correct for compile

--- src/ast_build/tagstokensmonitor/check_alias_ttm.c
+++ src/ast_build/tagstokensmonitor/check_alias_ttm.c
@@ -14,22 +14,22 @@
 ** - (type) value : descibe output.
 */
 
-t_bool		check_aliases(t_tags_tokens_monitor *self)
+t_bool		check_aliases_ttm(t_tags_tokens_monitor *self)
 {
 	char	*result_alias;
 	t_bool	assignation;
 
 	result_alias = "";
-	assignation = self->i + 1 < self->tt->length && \
+	assignation = self->i + 1 < (int)self->tt->length && \
 				  in(find_next_token(self->tt, self->i + 1, false), \
 						  "ASSIGNATION_EQUAL", "CONCATENATION", NULL);
 	if (!assignation && self->begin_cmd && \
 			(search_value(g_alias, self->token) && \
-			 !search_value(g_passed_alias, self->token)))
+			 !in_pylst_chare(self->token, g_passed_alias)))
 	{
 		result_alias = search_value(g_alias, self->token);
 		self->begin_cmd = ft_isspace(result_alias[ft_strlen(result_alias) - 1]);
-		push_pylst(g_passed_alias, self.token, 0, _ptr); // Check memory.
+		push_pylst(&g_passed_alias, self->token, 0, _ptr); // Check memory.
 		replace_alias_tagstokens(self->tt, result_alias, self->i);
 		if (self->begin_cmd)
 		{
