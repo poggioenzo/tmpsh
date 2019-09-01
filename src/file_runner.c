98dd6864e5be5e7fdbb0df212a052221e3b3707f
AbcSxyZ <rossi.sim@outlook.com>
2019-09-01 21:33:41 +0200

    change way to read file

--- src/file_runner.c
+++ src/file_runner.c
@@ -4,6 +4,35 @@
 #include "run_ast.h"
 #include "termios_setter.h"
 #include "ast.h"
+#include "exec_file.h"
+#include <fcntl.h>
+
+/*
+** check_rights:
+**
+** When the shell research exepected executable, check
+** if it's possible to run the given file.
+** Check permission + file existence.
+*/
+
+static t_bool	check_rights(char *cmd)
+{
+	char	*err_msg;
+
+	err_msg = NULL;
+	if (access(cmd, F_OK) == -1)
+		err_msg = NAME_SH" No such file or directory : %s\n";
+	else if (is_directory(cmd))
+		err_msg = NAME_SH" %s: is a directory\n";
+	else if (access(cmd, R_OK) == -1 || access(cmd, R_OK) == -1)
+		err_msg = NAME_SH" permission denied: %s\n";
+	if (err_msg)
+	{
+		ft_dprintf(STDERR_FILENO, err_msg, cmd);
+		return (false);
+	}
+	return (true);
+}
 
 /*
 ** run_file:
@@ -16,8 +45,12 @@ static int		run_file(char *filename)
 {
 	char			*content;
 	t_tagstokens	*tagstoks;
+	int				file_fd;
 
-	if(!(content = readfile(filename)))
+	if (!check_rights(filename))
+		return (1);
+	file_fd = open(filename, O_RDONLY);
+	if(!(content = fd_readfile(file_fd)))
 		return (ft_dprintf(2, NAME_SH" Error with %s\n", filename));
 	tagstoks = NULL;
 	input_init_tagstokens(&tagstoks, content);
