d801966cbfd22576463ff69735b5a8f757e3eed8
Enzo POGGIO <poggio.enzo@gmail.com>
2019-09-02 11:53:14 +0200

    norm 25 lignes

--- src/ast_build/tagstokensmonitor/check_alias_ttm.c
+++ src/ast_build/tagstokensmonitor/check_alias_ttm.c
@@ -14,28 +14,30 @@
 ** - (type) value : descibe output.
 */
 
+static t_bool get_assgt(t_tags_tokens_monitor *self, int up, int down, t_bool isdown)
+{
+	return ( up > down &&
+			in(find_prev_token(self->tt, isdown ? down : up, false), \
+				"ASSIGNATION_EQUAL", "CONCATENATION", NULL));
+}
+
 t_bool		check_aliases_ttm(t_tags_tokens_monitor *self)
 {
-	char	*result_alias;
+	char	*ret_alias;
 	t_bool	assignation;
 
-	result_alias = "";
-	assignation = self->i + 1 < (int)self->tt->length && \
-				  in(find_next_token(self->tt, self->i + 1, false), \
-						  "ASSIGNATION_EQUAL", "CONCATENATION", NULL);
+	ret_alias = "";
+	assignation = get_assgt(self, (int)self->tt->length, self->i + 1, true);
 	if (!assignation && self->begin_cmd && self->token\
 			&& (search_value(g_alias, self->token) && \
 			 !in_pylst_chare(self->token, g_passed_alias)))
 	{
-		result_alias = search_value(g_alias, self->token);
-		self->begin_cmd = ft_isspace(result_alias[ft_strlen(result_alias) - 1]);
+		ret_alias = search_value(g_alias, self->token);
+		self->begin_cmd = ft_isspace(ret_alias[ft_strlen(ret_alias) - 1]);
 		push_pylst(&g_passed_alias, self->token, 0, _ptr); // Check memory.
-		replace_alias_tagstokens(self->tt, result_alias, self->i);
+		replace_alias_tagstokens(self->tt, ret_alias, self->i);
 		if (self->begin_cmd)
-		{
-			reset_ttm(self);
-			return (true);
-		}
+			reset_ttm_out(self, true);
 	}
 	else if (assignation)
 	{
@@ -44,7 +46,5 @@ t_bool		check_aliases_ttm(t_tags_tokens_monitor *self)
 		else
 			return (true);
 	}
-	return (self->i - 1 > 0 &&
-			in(find_prev_token(self->tt, self->i - 1, false), \
-				"ASSIGNATION_EQUAL", "CONCATENATION", NULL));
+	return (get_assgt(self, self->i - 1, 0, false));
 }
