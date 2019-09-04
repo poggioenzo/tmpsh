f916cbd5bdf1605d1b2dc95639075a2eebc22d4c
AbcSxyZ <rossi.sim@outlook.com>
2019-09-04 11:25:10 +0200

    norm

--- include/ast_build/tagstokensmonitor/reset_ttm.h
+++ include/ast_build/tagstokensmonitor/reset_ttm.h
@@ -14,7 +14,7 @@
 #ifndef RESET_TTM_H
 # define RESET_TTM_H
 
-void reset_ttm(t_tags_tokens_monitor *self);
-int reset_ttm_out(t_tags_tokens_monitor *self, int out);
+void	reset_ttm(t_tags_tokens_monitor *self);
+int		reset_ttm_out(t_tags_tokens_monitor *self, int out);
 
 #endif
--- include/configuration/file_rights.h
+++ include/configuration/file_rights.h
@@ -17,10 +17,9 @@
 char	*check_rights(char *filename, int opts, t_bool free, \
 		t_bool print_error);
 
-# define	R	0b0001
-# define	W	0b0010
-# define	X	0b0100
-# define	F	0b1000
-
+# define R	0b0001
+# define W	0b0010
+# define X	0b0100
+# define F	0b1000
 
 #endif
