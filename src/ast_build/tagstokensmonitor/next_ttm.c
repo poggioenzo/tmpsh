5f81cc6cda9fbb367584a3c7d60189be13066fdf
Enzo POGGIO <poggio.enzo@gmail.com>
2019-09-04 16:18:39 +0200

    delete items will destroy token pointer

--- src/ast_build/tagstokensmonitor/next_ttm.c
+++ src/ast_build/tagstokensmonitor/next_ttm.c
@@ -36,8 +36,11 @@ t_bool	next_ttm(t_tags_tokens_monitor *self, t_bool clear)
 		self->tag = vindex_pylst(self->tt->tags, self->i);
 		self->token = vindex_pylst(self->tt->tokens, self->i);
 		if (clear)
+		{
+			self->token = ft_strdup(self->token);
 			self->i = delitems_tagstokens(self->tt, self->i,
 											self->i + 1, self->i - 1);
+		}
 	}
 	return (ret);
 }
