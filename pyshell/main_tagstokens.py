edbd3133c0269c7651a9d1c42cd2e85829d32f54
Enzo POGGIO <poggio.enzo@gmail.com>
2019-09-05 08:37:06 +0200

    stupid test

--- pyshell/main_tagstokens.py
+++ pyshell/main_tagstokens.py
@@ -7,37 +7,16 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         # ENTRY = "ls -l; echo 'lol' 1;$(echo ls) \\\ncat ../*"
         ENTRY = "echo $PATH\\ "
-        ENTRY = ''' ls
-<< EOF << EOF
-'quote'
-$('quote " ')
-EOF
-
-
-
-
-
-    lol
-
-
-    "dquote" $("$(dquote)"; (lol)) stmt
-EOF
-<<- $(  LOL  ) <<- 'EOF'
-
-
-\t\t
-     lol
-
-
-
-        $(  LOL  )
-        python3 lol.py $(
-    EOF
-global_var
-'''
+        ENTRY = '''
+        echo ~ ~te~st" ~ '$USER \""+\\$USER+$US\ER~$USERS' ~ t"e$USER \\'~'' ""'`' ""' \\' ""'" \\'"
+        '''
         # ENTRY = '<<-EOF\n\t lol\n\t\tlol\nEOF'
         # print(ENTRY)
     else:
         ENTRY = " ".join(sys.argv[1:])
     TAGSTOKENS = TagsTokens().init_with_input(ENTRY).check_syntax()
+    i = 0
+    while i < TAGSTOKENS.length:
+        print([TAGSTOKENS.tags[i], TAGSTOKENS.tokens[i]])
+        i += 1
     print(TAGSTOKENS)
