5a09375503cd4596040d6c8d7741ac64474c1eaa
AbcSxyZ <rossi.sim@outlook.com>
2019-10-16 21:09:00 +0200

    ignore signal handling for long buffer

--- src/configuration/reset_signals.c
+++ src/configuration/reset_signals.c
@@ -23,3 +23,13 @@ void	reset_signals(void)
 	signal(SIGTTOU, SIG_DFL);
 	signal(SIGWINCH, SIG_DFL);
 }
+
+void	ignore_prompt_signals(void)
+{
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGTSTP, SIG_IGN);
+	signal(SIGCONT, SIG_IGN);
+	signal(SIGTTIN, SIG_IGN);
+	signal(SIGTTOU, SIG_IGN);
+}
