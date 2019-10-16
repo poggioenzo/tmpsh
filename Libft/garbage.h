a7dda1bd441143fde595991e78a442526b2c0b4b
Enzo POGGIO <poggio.enzo@gmail.com>
2019-10-16 17:04:09 +0200

    for leaks

--- Libft/garbage.h
+++ Libft/garbage.h
@@ -20,8 +20,11 @@
 ** Macro to use for allocation and free.
 */
 
-# define MALLOC		garbage_malloc
-# define FREE		garbage_free
+// # define MALLOC		garbage_malloc
+// # define FREE		garbage_free
+
+# define MALLOC		malloc
+# define FREE		free
 
 /*
 ** Structure of the chained list to store each allocated element.
