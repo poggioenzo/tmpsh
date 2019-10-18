8e5ca53427e3af29fd3225671f35a11276eef704
Enzo POGGIO <poggio.enzo@gmail.com>
2019-10-18 12:09:46 +0200

    rm redondant buggy code

--- src/ast_build/tagstokensmonitor/is_stmt_ttm.c
+++ src/ast_build/tagstokensmonitor/is_stmt_ttm.c
@@ -13,14 +13,7 @@
 
 #include "ttm.h"
 
-void	remove_escape_token_ttm(t_tags_tokens_monitor *self)
-{
-	if (self->token && self->token[0] == *g_grammar->escape)
-		ft_strcpy(self->token, self->token + 1);
-}
-
 void	is_stmt_ttm(t_tags_tokens_monitor *self)
 {
 	self->begin_cmd = check_aliases_ttm(self);
-	remove_escape_token_ttm(self);
 }
