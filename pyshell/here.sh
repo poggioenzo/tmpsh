05c780f19d2b7242be641bd9da3f70784b9a9b03
Enzo POGGIO <poggio.enzo@gmail.com>
2019-10-18 12:12:21 +0200

    new test

--- pyshell/here.sh
+++ pyshell/here.sh
@@ -2,27 +2,24 @@ cat <<- EF1
 	content1
 	$PATH
 	EF1
-EF1
 
 cat << EF2
 	content2
-	content2.0
+	$PATH
 	EF2
 EF2
 
-cat << 'EF3'
+cat <<- 'EF3'
 	content3
 	$PATH
 	EF3
-'EF3'
-kill -9 $PPID
+
 cat << "EF4"
 	content4
 	$PATH
-	$(cat  << EF5
-	$(kill -9 $PPID)
-EF5
-	)
-	${PATH}
-	EF4
-"EF4"
+EF4
+
+# cat << $(EF5)
+# 	content5
+# 	$PATH
+# $(EF5)
