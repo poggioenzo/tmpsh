4b91f3f73dfd752c674de2ff3308294037ded824
Enzo POGGIO <poggio.enzo@gmail.com>
2019-10-28 17:00:50 +0100

    invalid read remove

--- src/ast_build/tagstokens/str_command_tagstokens.c
+++ src/ast_build/tagstokens/str_command_tagstokens.c
@@ -18,20 +18,21 @@ char	*str_command_tagstokens(t_tagstokens *self)
 	char	*tok;
 	char	*tag;
 	char	*str;
+	char	*local;
 
 	str = ft_strnew(0);
 	while (iter_tagstokens(self, &tok, &tag))
 	{
+		local = " ";
 		if (ft_strequ("SPACES", tag))
-			str = free_join(str, " ", FALSE);
+			str = ft_fstrjoin(&str, &local, TRUE, FALSE);
 		else if (ft_strequ("SUBAST", tag))
 		{
-			str = free_join(str, YELLOW, FALSE);
-			str = free_join(str, tok, FALSE);
-			str = free_join(str, WHITE, FALSE);
+			local = YELLOW"subast"WHITE;
+			str = ft_fstrjoin(&str, &local, TRUE, FALSE);
 		}
 		else
-			str = free_join(str, tok, FALSE);
+			str = ft_fstrjoin(&str, &tok, TRUE, FALSE);
 	}
 	return (str);
 }
